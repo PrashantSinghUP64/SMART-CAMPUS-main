bb5686e2bcf63f4e43b7586330164fa7a2456881
Prashant Kumar Singh <ps7027804@gmail.com>
2026-05-31 21:43:51 +0530

    Update server.js

--- SMART-CAMPUS-main/Working Model/server.js
+++ SMART-CAMPUS-main/Working Model/server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
